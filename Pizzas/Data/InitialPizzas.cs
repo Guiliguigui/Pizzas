1d39ec4fb75e9439f7092bd3fa4e4b9d5499639c
Guiliguigui <gui.mairesse@gmail.com>
2022-04-27 15:31:15 +0200

    Ajoutez des fichiers projet.

---
+++ Pizzas/Data/InitialPizzas.cs
@@ -0,0 +1,19 @@
+﻿using Pizzas.Models;
+using System.Collections.Generic;
+
+namespace Pizzas.Data
+{
+    public static class InitialPizzas
+    {
+        public static List<Pizza> Pizzas = new List<Pizza>()
+        {
+            new Pizza{ Id =1, Name ="Bacon", Price = 12, Ingredients = new[] { "bacon", "mozzarella", "champignon", "emmental" }, ImageName = "bacon.jpg"  },
+            new Pizza{ Id =2, Name ="4 fromages", Price= 11, Ingredients = new[] { "cantal", "mozzarella", "fromage de chèvre", "gruyère" }, ImageName = "cheese.jpg"  },
+            new Pizza{ Id =3, Name ="Margherita", Price = 10, Ingredients = new[] { "sauce tomate", "mozzarella", "basilic" }, ImageName = "margherita.jpg"  },
+            new Pizza{ Id =4, Name ="Mexicaine", Price=12, Ingredients = new[] { "boeuf", "mozzarella", "maïs", "tomates", "oignon", "coriandre" }, ImageName = "meaty.jpg"  },
+            new Pizza{ Id =5, Name ="Reine", Price=11, Ingredients = new[] { "jambon", "champignons", "mozzarella" }, ImageName = "mushroom.jpg"  },
+            new Pizza{ Id =6, Name ="Pepperoni", Price=11, Ingredients = new[] { "mozzarella", "pepperoni", "tomates" }, ImageName = "pepperoni.jpg"  },
+            new Pizza{ Id =7, Name ="Végétarienne",Price = 10, Ingredients = new[] { "champignons", "roquette", "artichauts", "aubergine" }, ImageName = "veggie.jpg"  },
+        };
+    }
+}
---
+++ Pizzas/Pages/PizzaComponent.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pizzas.Data;
+using Pizzas.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Pizzas.Pages
+{
+    public class PizzaBase : ComponentBase
+    {
+        protected bool IsAdminMode { get; set; } = false;
+        protected Dictionary<Pizza, int> Cart { get; set; } = new Dictionary<Pizza, int>();
+        protected decimal Total => Cart.Sum(pizza => pizza.Key.Price * pizza.Value);
+        protected List<Pizza> PizzaList { get; set; }
+        protected Pizza PizzaToEdit { get; set; } = null;
+        protected string PizzaToEditIngredientsString { get; set; } = "";
+        protected PizzaBase()
+        {
+            PizzaList = InitialPizzas.Pizzas;
+        }
+        protected void AddToCart(Pizza pizza)
+        {
+            if (Cart.ContainsKey(pizza))
+                Cart[pizza]++;
+            else
+                Cart.Add(pizza, 1);
+        }
+        protected void RemoveFromCart(Pizza pizza)
+        {
+            if (Cart[pizza] == 1)
+                Cart.Remove(pizza);
+            else
+                Cart[pizza]--;
+        }
+        protected void EmptyCart()
+        {
+            Cart.Clear();
+        }
+
+        protected void EditPizza(Pizza pizza)
+        {
+            PizzaToEdit = pizza;
+            PizzaToEditIngredientsString = string.Join(", ", PizzaToEdit.Ingredients);
+        }
+        protected void SubmitPizza()
+        {
+            PizzaToEdit.Ingredients = PizzaToEditIngredientsString.Split(", ");
+            PizzaToEdit = null;
+
+        }
+    }
+}
